4dc39214c5aebc035c68798979a3a2bef2794b7f
Yu <kgadbcksrb@gmail.com>
2024-03-07 21:01:45 +0900

    feat : 스크립터블 오브젝트 만드는중

--- Assets/_Scripts/Data/ScriptableObject/MonsterData.cs
+++ Assets/_Scripts/Data/ScriptableObject/MonsterData.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,5 +6,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "MonsterData", menuName = "Data/Monster")]
 public class MonsterData : ScriptableObject
 {
-    
+    public string name;
+    public string description;
+
+    public Monster prefab;
+
+
+    [Serializable]
+    public class MonsterInfo
+    {
+        
+    }
+   
 }
